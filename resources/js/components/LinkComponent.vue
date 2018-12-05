59859d2244b5c4b2c2ef05f71074e255ae5f76cb
NRD3v <nr.dev.contact@gmail.com>
2018-12-05 15:45:00 +0300

    Adding bold on link selection + store side

--- resources/js/components/LinkComponent.vue
+++ resources/js/components/LinkComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <h6>{{ cityName }}</h6>
         <ul>
-            <li class="small" v-for="practice in city">
+            <li class="small" v-for="practice in city" :class="{highlight:cityName + practice === linkHighlighted}">
                 <a href="#" @click="onClick(cityName, practice)">{{ practice }}</a>
             </li>
         </ul>
@@ -10,12 +10,24 @@
 </template>
 
 <script>
+    import { store } from '../store/index'
     export default {
+        computed: {
+            linkHighlighted() {
+                return store.links.getters.linkHighlighted
+            }
+        },
         methods: {
           onClick(cityName, practice) {
+              store.links.dispatch('setLinkHighlight', cityName + practice);
               this.$emit('onLinkClick', [cityName, practice])
           }
         },
         props: ['city', 'cityName']
     }
 </script>
+<style scoped>
+    li.highlight {
+        font-weight: bold;
+    }
+</style>
--- resources/js/store/links.js
+++ resources/js/store/links.js
@@ -4,19 +4,24 @@ Vue.use(Vuex);
 
 export const links = new Vuex.Store({
     state: {
-        links: []
+        links: [],
+        linkHighlighted: undefined
     },
     getters: {
-        links: state => state.links
+        links: state => state.links,
+        linkHighlighted: state => state.linkHighlighted
     },
     mutations: {
         setLinksList(state) {
             return window.axios.get('/links').then(response => {
                 state.links = !response.data.empty ? response.data : []
             });
-        }
+        },
+        setLinkHighlighted: (state, payload) => state.linkHighlighted = payload
+
     },
     actions: {
-        setLinks: (context) => context.commit('setLinksList')
+        setLinks: (context) => context.commit('setLinksList'),
+        setLinkHighlight: (context, payload) => context.commit('setLinkHighlighted', payload)
     }
 });
